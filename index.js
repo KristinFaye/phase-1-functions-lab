9843b332c1c39c39e6ce3dc97357e552d4f1859a
Kristin <kristinfaye.asuanobrien@gmail.com>
2021-11-03 12:11:04 -0500

    commit tree d582978dec059716b7d13b181e36efacee0881ac

--- index.js
+++ index.js
@@ -1,50 +1,34 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(origin){
-const address = 42;
-console.log (origin,address)
-if (origin > address){
+function distanceFromHqInBlocks(origin) {
+  const address = 42;
+  console.log(origin, address);
+  if (origin > address) {
     return origin - address;
-}
-else if (address > origin){
+  } else if (address > origin) {
     return address - origin;
+  }
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(origin) {
+  return distanceFromHqInBlocks(origin) * 264;
 }
 
-}
- distanceFromHqInBlocks()
+distanceFromHqInFeet();
 
-function distanceFromHqInFeet(origin){
-
-    return distanceFromHqInBlocks(origin) * 264;
-
-
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end)*264;
 }
 
-distanceFromHqInFeet()
-
-
- 
-
-
- function distanceTravelledInFeet(start, end){
-     if (start < 42){
-        return(start-end)*264;
-    } else{
-        return(end - start)*264;
-    }
- }
- 
-
- 
-    function calculatesFarePrice(start,end){
-        const feet = distanceTravelledInFeet(start, end)
-        if (feet <=  400){
-            return 0
-        }else if (feet > 400 && feet <=2000 ){
-            return (feet - 400) * .02 
-        }else if (feet > 2000 && feet < 2500){
-            return 25
-        }else{
-            return 'cannot travel that far'
-        }
-    }
- 
+function calculatesFarePrice(start, end) {
+  const feet = distanceTravelledInFeet(start, end);
+  if (feet <= 400) {
+    return 0;
+  } else if (feet > 400 && feet <= 2000) {
+    return (feet - 400) * 0.02;
+  } else if (feet > 2000 && feet < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
